1a2698a25d5df847e27b1ea6eaf7292ee68a139a
Androgalaxi <gtaprokills@outlook.com>
2026-08-02 00:43:00 -0400

    update for database

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 database = sqlite3.connect("AetherX.db")
 cursor = database.cursor()
-cursor.excecute("CREATE TABLE IF NOT EXISTS")
+
 if not TOKEN:
     raise RuntimeError("TOKEN environment variable is not set")
 
