9a328054a90e745748a15be1d0fed36ef879d22d
Androgalaxi <gtaprokills@outlook.com>
2026-08-04 02:20:14 -0400

    im trying my best - wintersnowe

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -10,14 +10,13 @@ import bot
 class Birthday(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        database = sqlite3.connect('AetherX.db')
+        cursor = database.cursor()
 
         @commands.hybrid_command(name="birthday", description="Set your birthday")
         async def birthday(self, ctx: commands.Context, date: str, timezone: str = "UTC"):
             """Set your birthday"""
             try:
-                database = sqlite3.connect('AetherX.db')
-                cursor = database.cursor()
                 cursor.execute("INSERT OR REPLACE INTO birthdays (user_id, birthday, timezone) VALUES (?, ?, ?)", (ctx.author.id, date, timezone))
                 database.commit()
                 await ctx.send(f"Your birthday has been set to {date} in timezone {timezone}.")
