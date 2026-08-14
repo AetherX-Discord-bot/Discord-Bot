b7047bfc814be454cc2d0389226c54650e3fc3de
Androgalaxi <gtaprokills@outlook.com>
2026-08-14 08:54:14 -0400

    Update update.py and bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ bot = AetherXBot(command_prefix="$", intents=intents)
 @bot.event
 async def on_ready():
     animator = BootAnimator()
-    await bot.change_presence(activity=discord.Game(name="Version 0.2.0 Alpha out now | $help"))
+    await bot.change_presence(activity=discord.Game(name="Version 0.2.5 Alpha out now | $help"))
     database = sqlite3.connect("AetherX.db")
     cursor = database.cursor()
     
--- cogs/update.py
+++ cogs/update.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-version = "0.2.0-alpha"
+version = "0.2.5-alpha"
 class Updates(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,7 +10,7 @@ class Updates(commands.Cog):
     @commands.hybrid_command(name="updates", description="View the latest updates and changes to AetherX.")
     async def updates(self, ctx: commands.Context):
         embed = discord.Embed(title="AetherX Update Log", description=f"Version {version} - Latest Updates and Changes", color=discord.Color.blue())
-        embed.add_field(name="New Features", value="- Added music playback supporting YouTube, SoundCloud, Bandcamp, Twitch, and Vineo", inline=False)
+        embed.add_field(name="New Features", value="- Added honeypot integration", inline=False)
         embed.add_field(name="Bug Fixes", value="- No new bug fixes at this time", inline=False)
         await ctx.send(embed=embed)
 
