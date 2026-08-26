012f2763934a7c0fbfb1fb8facdbdc19bc64324a
Androgalaxi <gtaprokills@outlook.com>
2026-08-26 00:54:14 -0400

    Update about.py

--- cogs/about.py
+++ cogs/about.py
@@ -26,7 +26,7 @@ class AboutCog(commands.Cog):
         )
         about_embed.add_field(
             name="Bot Version",
-            value="0.2.0-alpha",
+            value="0.3.0-alpha",
             inline=False
         )
         about_embed.add_field(
